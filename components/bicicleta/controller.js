75f95fc548f5cd684082e903d1c526ba04a03670
Cristian Franco <cfrancobedoya@gmail.com>
2021-09-24 23:49:33 -0500

    Changing functions

--- components/bicicleta/controller.js
+++ components/bicicleta/controller.js
@@ -5,15 +5,6 @@ function listBicycles() {
     return store.list();
 }
 
-function updateBicycleById(id, user) {
-    return new Promise(async (resolve, reject) => {
-        const result = await store.updateById(id, user);
-
-        resolve(result);
-    })
-}
-
 module.exports = {
     listBicycles,
-    updateBicycleById,
 }
--- components/bicicleta/network.js
+++ components/bicicleta/network.js
@@ -6,7 +6,6 @@ const router = express.Router();
 
 // END-POINTS
 router.get('/', getBicycles);
-router.patch('/:id', updateBicycle);
 
 function getBicycles(req, res) {
     controller.listBicycles()
@@ -17,15 +16,5 @@ function getBicycles(req, res) {
             response.error(req, res, 'Internal error', 500, err);
         });
 };
-
-function updateBicycle(req, res) {
-    controller.updateBicycleById(req.params.id, req.body.user)
-        .then((data) => {
-            response.success(req, res, data, 200);
-        })
-        .catch(e => {
-            response.error(req, res, 'Internal Error', 500, e);
-        });
-}
 
 module.exports = router;
